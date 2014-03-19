e12d98de8aecf86c4a2c7b1737694aa9a04daa37
nitroluke <nitro.luke@gmail.com>
2014-03-19 00:26:01 -0600

    more formatting changes and cleanup

--- findWord.c
+++ findWord.c
@@ -21,11 +21,7 @@ int * findWord(char charArr[arrSize][arrSize], char searchFor[3]){
 
 int arrI = 0;
 int arrJ = 0;
-int true = 1;
-int false = 0;
-int hasWord = false;
 
-if(!hasWord){
 addressArr[0] = -1;
 addressArr[1] = -1;
 for(arrI; arrI < arrSize; arrI++){ // search for horizontal words words.
@@ -33,7 +29,6 @@ for(arrI; arrI < arrSize; arrI++){ // search for horizontal words words.
 		if(charArr[arrI][1] == searchFor[1]){
 			if(charArr[arrI][2] == searchFor[2]){
 			addressArr[0] = charArr[0][arrI];  // the word exists
-			hasWord = true; // set hasWord = to true
 			}
 		}
 	}
@@ -43,12 +38,11 @@ for(arrJ; arrJ < arrSize; arrJ++){
 		if(charArr[1][arrJ] == searchfor[1]){
 			if(charArr[2][arrJ] == searchFor[2]){
 			addressArr[1] = charArr[arrJ][0]; // the word is vertical
-			hasWord = true; // set hasWord = true
 			}
 		}
 	}
 }
-}
+
 
 
 return addressArr;
