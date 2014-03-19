5787977f6877039ce63ffcf2fd010615d85546b8
nitroluke <nitro.luke@gmail.com>
2014-03-18 20:12:39 -0600

    formatting and code cleanup

--- findWord.c
+++ findWord.c
@@ -10,36 +10,24 @@ in lab 9
 int arrSize = 3;
 static int addressArr[2];
 
-int * findWord(char charArr[arrSize][arrSize], int arrIndex, char searchFor[3]){
-/* char foundWord[arrSize] = "   "; */
+int main(){
+
+
+return 0;
+}
+
+
+int * findWord(char charArr[arrSize][arrSize], char searchFor[3]){
+
 int arrI = 0;
 int arrJ = 0;
 int true = 1;
 int false = 0;
 int hasWord = false;
-/* int wordCoordI = 0;
-int wordCoordJ = 0; */
 
-
-/* for(arrI; arrI < arrSize; arrI++){
-	for(arrJ; arrJ < arrSize;){
-	
-	if(!hasWord){
-	hasWord = true;
-	arrJ = 0;
-	// make wordCoord -1, -1 because a word was not found
-	break;
-	}
-	else if(charArr[arrI][arrJ] == str1[arrJ]){
-	 arrJ++;
-	}
-	else if(arrJ = (arrSize - 1)){
-	
-	}
-	}
-	wordCoordI++; //put this and wordCoordJ into addressArr[] because a word was found somehow
-} */
-if(hasWord){
+if(!hasWord){
+addressArr[0] = -1;
+addressArr[1] = -1;
 for(arrI; arrI < arrSize; arrI++){ // search for horizontal words words.
 	if(charArr[arrI][0] == searchFor[0]){
 		if(charArr[arrI][1] == searchFor[1]){
@@ -61,10 +49,7 @@ for(arrJ; arrJ < arrSize; arrJ++){
 	}
 }
 }
-else{
-addressArr[0] = -1;
-addressArr[1] = -1;
-}
+
 
 return addressArr;
 }
